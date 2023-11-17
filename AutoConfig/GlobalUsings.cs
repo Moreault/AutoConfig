a537b6aa25ebdd2bf26c6531706dcd89cd005dc9
Mathieu Moreault <seranx@gmail.com>
2023-11-17 10:17:30 -0500

    Added an option to use AutoConfig on specific assemblies rather than all of them (AutoInject has the same option in 2.2.0)

--- AutoConfig/GlobalUsings.cs
+++ AutoConfig/GlobalUsings.cs
@@ -3,5 +3,6 @@
 global using System.Reflection;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Options;
 global using ToolBX.Reflection4Humans.Extensions;
 global using ToolBX.Reflection4Humans.TypeFetcher;
--- AutoConfig/ServiceCollectionExtensions.cs
+++ AutoConfig/ServiceCollectionExtensions.cs
@@ -2,10 +2,33 @@
 
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Adds all classes with the <see cref="AutoConfigAttribute"/> attribute from the specified assembly to the <see cref="IServiceCollection"/> as <see cref="IOptions{TOptions}"/>.
+    /// </summary>
+    public static IServiceCollection AddAutoConfig(this IServiceCollection services, Assembly assembly, IConfiguration configuration)
+    {
+        if (services is null) throw new ArgumentNullException(nameof(services));
+        if (assembly is null) throw new ArgumentNullException(nameof(assembly));
+        if (configuration is null) throw new ArgumentNullException(nameof(configuration));
+
+        var types = Types.From(assembly).Where(x => !x.IsInterface && !x.IsAbstract && !x.IsGenericTypeDefinition && !x.IsGenericType && x.HasAttribute<AutoConfigAttribute>());
+        return services.AddAutoConfig(configuration, types);
+    }
+
+    /// <summary>
+    /// Adds all classes with the <see cref="AutoConfigAttribute"/> attribute from all assemblies to the <see cref="IServiceCollection"/> as <see cref="IOptions{TOptions}"/>.
+    /// </summary>
     public static IServiceCollection AddAutoConfig(this IServiceCollection services, IConfiguration configuration)
     {
-        var types = Types.Where(x => !x.IsInterface && !x.IsAbstract && !x.IsGenericTypeDefinition && !x.IsGenericType && x.HasAttribute<AutoConfigAttribute>()).ToList();
+        if (services is null) throw new ArgumentNullException(nameof(services));
+        if (configuration is null) throw new ArgumentNullException(nameof(configuration));
 
+        var types = Types.Where(x => !x.IsInterface && !x.IsAbstract && !x.IsGenericTypeDefinition && !x.IsGenericType && x.HasAttribute<AutoConfigAttribute>());
+        return services.AddAutoConfig(configuration, types);
+    }
+
+    private static IServiceCollection AddAutoConfig(this IServiceCollection services, IConfiguration configuration, IEnumerable<Type> types)
+    {
         foreach (var type in types)
         {
             var attribute = (AutoConfigAttribute)Attribute.GetCustomAttribute(type, typeof(AutoConfigAttribute), true)!;
